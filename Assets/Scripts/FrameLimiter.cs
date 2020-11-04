e5b516f9e89806542ecce123fd4ddde5f0cbf10c
Xplicit343 <xplicit343@gmail.com>
2020-11-04 21:01:13 +0000

    added a frame limiter until i actually remake all coroutines to use time instead of endOfFrame

---
+++ Assets/Scripts/FrameLimiter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLimiter : MonoBehaviour
+{
+    public int target = 120;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
+    }
+
+    void Update()
+    {
+        if (Application.targetFrameRate != target)
+            Application.targetFrameRate = target;
+    }
+}
