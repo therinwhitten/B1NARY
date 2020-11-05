17b387aae94d360ce7521f076fd77ad654488140
Xplicit343 <xplicit343@gmail.com>
2020-11-05 13:45:53 +0000

    added Text Mesh Pro

---
+++ Assets/Scripts/FrameLimiter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLimiter : MonoBehaviour
+{
+    // DON'T PUT THIS IN THE FINAL BUILD!!!!!!!!!!!!
+    public int target = 120;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
+    }
+
+    void Update()
+    {
+        if (Application.targetFrameRate != target)
+            Application.targetFrameRate = target;
+    }
+}
