629267235fe0e779c0b17c6f55a0aa1a0fcca6ce
Xplicit343 <xplicit343@gmail.com>
2020-12-09 17:29:00 +0000

    moved UI to assets for git

---
+++ Assets/UI/Scrolling BG/ScrollBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBG : MonoBehaviour
+{
+    public float speed = 5f;
+    private RectTransform BGPattern;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGPattern = transform.GetChild(0).GetComponent<RectTransform>();
+        Debug.Log(BGPattern.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = BGPattern.anchoredPosition * 100;
+        BGPattern.anchoredPosition = Vector3.MoveTowards(BGPattern.anchoredPosition, targetPos, speed * Time.deltaTime);
+    }
+}
