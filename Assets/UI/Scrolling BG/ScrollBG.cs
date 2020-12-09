db5bc7e7733add97b8b2531e7217b087589dc23a
Xplicit343 <xplicit343@gmail.com>
2020-12-09 17:55:36 +0000

    Scrolling BG implemented

--- Assets/UI/Scrolling BG/ScrollBG.cs
+++ Assets/UI/Scrolling BG/ScrollBG.cs
@@ -6,17 +6,24 @@ public class ScrollBG : MonoBehaviour
 {
     public float speed = 5f;
     private RectTransform BGPattern;
+    Vector2 initialPos, targetPos;
     // Start is called before the first frame update
     void Start()
     {
         BGPattern = transform.GetChild(0).GetComponent<RectTransform>();
         Debug.Log(BGPattern.name);
+        initialPos = BGPattern.anchoredPosition;
+        targetPos = new Vector2(BGPattern.anchoredPosition.x - BGPattern.rect.width, BGPattern.anchoredPosition.y - BGPattern.rect.height);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = BGPattern.anchoredPosition * 100;
+        if (BGPattern.anchoredPosition == targetPos)
+        {
+            BGPattern.anchoredPosition = initialPos;
+        }
         BGPattern.anchoredPosition = Vector3.MoveTowards(BGPattern.anchoredPosition, targetPos, speed * Time.deltaTime);
     }
 }
---
+++ Assets/UI/Scrolling BG/ScrollImage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollImage : MonoBehaviour
+{
+    public float speed = 50f;
+
+    private RectTransform rectTransform;
+    Vector2 initialPos, targetPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        Debug.Log(rectTransform.name);
+        initialPos = rectTransform.anchoredPosition;
+        targetPos = new Vector2(rectTransform.anchoredPosition.x - rectTransform.rect.width, rectTransform.anchoredPosition.y + rectTransform.rect.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rectTransform.anchoredPosition == targetPos)
+        {
+            rectTransform.anchoredPosition = initialPos;
+        }
+        rectTransform.anchoredPosition = Vector3.MoveTowards(rectTransform.anchoredPosition, targetPos, speed * Time.deltaTime);
+
+    }
+}
