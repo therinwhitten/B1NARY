46f867291e8a8f8beade1078f32bdb09c0bb7946
Xplicit343 <xplicit343@gmail.com>
2021-04-08 16:01:04 +0100

    voice line loading redone

--- Assets/Scripts/Dialogue/DialogueLine.cs
+++ Assets/Scripts/Dialogue/DialogueLine.cs
@@ -1,8 +1,11 @@
 public class DialogueLine
 {
-    public DialogueLine()
+    public string line;
+    public int index;
+    public DialogueLine(string line, int index)
     {
-
+        this.line = line;
+        this.index = index;
     }
 
 }
