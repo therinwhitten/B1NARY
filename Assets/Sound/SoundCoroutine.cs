5cf382b109bd25830d90b3eabff88f21d0585826
AnOddDoorKnight <gooeycuppy@live.com>
2022-05-25 07:29:40 -0700

    Added Meta Files and fixed all compiler issues

--- Assets/Sound/SoundCoroutine.cs
+++ Assets/Sound/SoundCoroutine.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SoundCoroutine
 {
 	public readonly AudioSource audioSource;
-    public readonly AudioMaster audioMaster;
+	public readonly AudioMaster audioMaster;
 	private Coroutine coroutine = null;
 	public bool destroyOnFinish = false, hasPlayed = false;
 
@@ -16,31 +16,31 @@ public class SoundCoroutine
 		(AudioListener audioListener, CustomAudioClip audioClip, AudioMaster audioMaster)
 	{
 		audioSource = audioListener.gameObject.AddComponent<AudioSource>();
-        this.audioMaster = audioMaster;
+		this.audioMaster = audioMaster;
 		AudioClip = audioClip;
 	}
 
-    public CustomAudioClip AudioClip
-    {
-        get
-        {
-            var clip = (CustomAudioClip)audioSource.clip;
-            clip.volume = audioSource.volume;
-            clip.pitch = audioSource.pitch;
-            return clip;
-        }
-        set
-        {
-            audioSource.clip = audioClip;
+	public CustomAudioClip AudioClip
+	{
+		get
+		{
+			var clip = (CustomAudioClip)audioSource.clip;
+			clip.volume = audioSource.volume;
+			clip.pitch = audioSource.pitch;
+			return clip;
+		}
+		set
+		{
+			audioSource.clip = value;
 
-            // This needs testing!
-            var random = new Random();
-		    audioSource.pitch = (float)(value.pitch - (value.pitchVariance * 3) 
-			    + (random.NextDouble() * value.pitchVariance * 3));
-		    audioSource.volume = (float)(value.volume - value.volumeVariance
-		    	+ (random.NextDouble() * value.volumeVariance));
-        }
-    }
+			// This needs testing!
+			var random = new Random();
+			audioSource.pitch = (float)(value.pitch - (value.pitchVariance * 3) 
+				+ (random.NextDouble() * value.pitchVariance * 3));
+			audioSource.volume = (float)(value.volume - value.volumeVariance
+				+ (random.NextDouble() * value.volumeVariance));
+		}
+	}
 
 	public void PlaySingle()
 	{
@@ -61,12 +61,11 @@ public class SoundCoroutine
 	}
 	private IEnumerator CloseCoroutineAwaiter()
 		{
-			// Better performance than checking every frame or half second
-            // I also want to specifically mention the base class instead of using Dynamic,
-            // - but i forget what its base class from.
-			Func<dynamic> classCreator = audioSource.clip.length > 5 ? 
-				(Func<dynamic>)(() => new WaitForSeconds(0.1f * audioSource.clip.length)) :
-				(Func<dynamic>)(() => new WaitForEndOfFrame());
+			// The casts are here to tell the primitive C# compiler to force them
+			// - not to be an anonymous delegates that generates a YieldInstruction
+			Func<YieldInstruction> classCreator = audioSource.clip.length > 5 ? 
+				(Func<YieldInstruction>)(() => new WaitForSeconds(0.1f * audioSource.clip.length)) :
+				(Func<YieldInstruction>)(() => new WaitForEndOfFrame());
 			while (audioSource.isPlaying)
 				yield return classCreator.Invoke();
 			Stop();
