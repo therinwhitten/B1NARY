1f8c9de09d9ab478fb1b04eac367423e21b5bd73
therinwhitten <71677706+therinwhitten@users.noreply.github.com>
2022-08-24 09:15:51 -0700

    Adjusted and Finised Templates for Character

--- Assets/Scripts/UI/Buttons/CreativeButtons.cs
+++ Assets/Scripts/UI/Buttons/CreativeButtons.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// This script takes all of the transparency out of an image being used as a button to enable only the image to be the hitbox///
+
 public class CreativeButtons : MonoBehaviour
 {
     // Start is called before the first frame update
