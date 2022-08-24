c9df8c8bfa65b11d825957ab10903141cd323a8d
therinwhitten <71677706+therinwhitten@users.noreply.github.com>
2022-08-24 06:22:48 -0700

    Added Creative Buttons Script. Ask me

---
+++ Assets/Scripts/UI/Buttons/CreativeButtons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreativeButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+
+  
+}
