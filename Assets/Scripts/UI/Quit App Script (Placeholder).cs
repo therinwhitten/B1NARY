f831c9e1f5e3049e592de5a261bd25fa0dc715ae
TherinWhitten <71677706+therinwhitten@users.noreply.github.com>
2022-10-05 06:41:50 -0700

    Added Shaders needed for buildsUI Almomst complete

---
+++ Assets/Scripts/UI/Quit App Script (Placeholder).cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Quitscript : MonoBehaviour
+{
+ 
+    void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
