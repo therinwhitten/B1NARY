b594973ee8fdafa06ef2d87328873ae8143fe44d
TherinWhitten <71677706+therinwhitten@users.noreply.github.com>
2022-10-20 08:09:19 -0700

    Main Menu UI Sounds but Display panel

--- Assets/Scripts/Sound/ButtonUIAudio.cs
+++ Assets/Scripts/Sound/ButtonUIAudio.cs
@@ -1,15 +1,24 @@
-﻿namespace B1NARY.Audio
+﻿namespace B1NARY.Audio 
 {
   using System.Collections;
   using System.Collections.Generic;
   using UnityEngine;
 
+   //Script for allowing custom sounds to be added to mouse movements. 
+
   public class ButtonUIAudio : MonoBehaviour
 
   {
+      //Section to add different options for audio files in the Component
+
     public AudioSource UISounds;
     public AudioClip hoverSound;
     public AudioClip clickSound;
+    public AudioClip releaseSound;
+    public AudioClip slideSound;
+    public AudioClip scrollSound;
+
+      //Section to allow you to choose a one shot, which allows custom audio files. 
 
     public void Hoversound() 
     {
@@ -18,6 +27,18 @@
     public void ClickSound() 
      {
         UISounds.PlayOneShot (clickSound);
+    }
+    public void ReleaseSound() 
+     {
+        UISounds.PlayOneShot (releaseSound);
+    }
+    public void SlideSound()
+    {
+       UISounds.PlayOneShot (slideSound);
+    }
+    public void ScrollSound()
+    {
+      UISounds.PlayOneShot (scrollSound);
     }
   }
 
