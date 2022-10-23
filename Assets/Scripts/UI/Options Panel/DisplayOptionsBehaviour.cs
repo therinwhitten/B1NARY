519af18fb3af606902c10f39bdbe7ee6b05b9ed6
TherinWhitten <71677706+therinwhitten@users.noreply.github.com>
2022-10-23 08:12:26 -0700

    Adding Graphic Levels to main Script

--- Assets/Scripts/UI/Options Panel/DisplayOptionsBehaviour.cs
+++ Assets/Scripts/UI/Options Panel/DisplayOptionsBehaviour.cs
@@ -10,15 +10,29 @@
 		public TMP_Dropdown hentaiDropdown;
 		public bool InFullScreen { get => Screen.fullScreen; set => Screen.fullScreen = value; }
 		public TMP_Dropdown fullScreenDropdown;
-		private void Awake()
+		public RenderPipelineAsset[] qualityLevels;
+        public TMP_Dropdown dropdown;
+
+        private void Awake()
 		{
 			hentaiDropdown.value = HentaiMode ? 1 : 0;
 			hentaiDropdown.onValueChanged.AddListener(ChangedHentaiValue);
 			fullScreenDropdown.value = InFullScreen ? 0 : 1;
 			fullScreenDropdown.onValueChanged.AddListener(ChangedFullScreenValue);
+			dropdown.value = QualitySettings.GetQualityLevel();
 		}
+	    
+        public void ChangeLevel(int value) //Graphic Levels (Need to Fix)
+		  {
+             QualitySettings.SetQualityLevel(value);
+             QualitySettings.renderPipeline = qualityLevels[value];
+          } 
 		public void ChangedHentaiValue(int option) => HentaiMode = option == 1;
 		public void ChangedFullScreenValue(int fullScreen) => InFullScreen = fullScreen == 0;
+		
+         
+		
+        
 
 	}
 }
