777e56b8a7e9b87659b766772eb19bcbc46c55bb
TherinWhitten <71677706+therinwhitten@users.noreply.github.com>
2022-10-23 19:48:46 -0700

    Broken Resolutions but added Graphic Display setti

--- Assets/Scripts/UI/Options Panel/DisplayOptionsBehaviour.cs
+++ Assets/Scripts/UI/Options Panel/DisplayOptionsBehaviour.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
 	using TMPro;
 	using UnityEngine;
+	using UnityEngine.UI;
 	using UnityEngine.Rendering;
 
 	public sealed class DisplayOptionsBehaviour : MonoBehaviour
@@ -13,18 +14,42 @@
 		public bool InFullScreen { get => Screen.fullScreen; set => Screen.fullScreen = value; }
 		public TMP_Dropdown fullScreenDropdown;
 		public RenderPipelineAsset[] qualityLevels;
-        public TMP_Dropdown dropdown;
+        public TMP_Dropdown qualityDropdown;
+		public TMPro.TMP_Dropdown resolutionDropdown;
+         Resolution[] resolutions;
 
+		 void Start ()
+		 {
+           resolutions = Screen.resolutions;
+			resolutionDropdown.ClearOptions();
+			List<string> options = new List<string>();
+			int currentResolutionIndex = 0;
+			for (int i = 0; i < resolutions.Length; i++)
+			 {
+			    string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+				options.Add(option);
+
+				if (resolutions[i].width == Screen.currentResolution.width &&
+				    resolutions[i].height == Screen.currentResolution.height)
+				{
+					currentResolutionIndex = i;
+				}						
+			 }
+			resolutionDropdown.AddOptions(resolutions);
+			resolutionDropdown.value = currentResolutionIndex;
+			resolutionDropdown.RefreshShownValue();
+		 }
         private void Awake()
 		{
 			hentaiDropdown.value = HentaiMode ? 1 : 0;
 			hentaiDropdown.onValueChanged.AddListener(ChangedHentaiValue);
 			fullScreenDropdown.value = InFullScreen ? 0 : 1;
 			fullScreenDropdown.onValueChanged.AddListener(ChangedFullScreenValue);
-			dropdown.value = QualitySettings.GetQualityLevel();
+			qualityDropdown.value = QualitySettings.GetQualityLevel();
+			
 		}
 	    
-    public void ChangeLevel(int value) // Graphics Quality
+     public void ChangeLevel(int value) // Graphics Quality
 	 {
         QualitySettings.SetQualityLevel(value);
         QualitySettings.renderPipeline = qualityLevels[value];
