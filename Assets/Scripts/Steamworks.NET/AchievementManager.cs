439feb77f4b3f29cf4190edd32aae05042c854d5
TherinWhitten <71677706+therinwhitten@users.noreply.github.com>
2023-08-07 10:02:11 -0700

    Bokeh, Translation Fixes, Closed Beta

--- Assets/Scripts/Steamworks.NET/AchievementManager.cs
+++ Assets/Scripts/Steamworks.NET/AchievementManager.cs
@@ -51,11 +51,25 @@
 		{
 			nut_cracker,
 			second_swallow,
+			intern_UwU,
+			male_route,
+			female_route, 
+			demo_female_hscene, 
+			demo_male_hscene,
+			demo_complete,
+			closed_beta_demo,
 		}
 		public static IReadOnlyList<SteamAchievement> AllAchievements { get; } = new SteamAchievement[]
 		{
 			new SteamAchievement(AchievementIndex.nut_cracker, "Nut Cracker", ""),
 			new SteamAchievement(AchievementIndex.second_swallow, "Second Swallow", ""),
+			new SteamAchievement(AchievementIndex.intern_UwU, "Intern", ""),
+			new SteamAchievement(AchievementIndex.male_route, "I REALLY want a cup of Coffee...", ""),
+			new SteamAchievement(AchievementIndex.female_route, "Jujitsu Training did NOT Pay off...", ""),
+			new SteamAchievement(AchievementIndex.demo_female_hscene, "Yuri between the sheets.", ""),
+			new SteamAchievement(AchievementIndex.demo_male_hscene, "Hentai Protag.", ""),
+			new SteamAchievement(AchievementIndex.demo_complete, "Cultured", ""),
+			new SteamAchievement(AchievementIndex.closed_beta_demo, "Beta Tester", ""),
 		};
 
 
